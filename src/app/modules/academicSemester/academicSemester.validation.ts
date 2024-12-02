1ef04ceaefc479653cc0c2b8d1e1add014be6e55
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-02 15:39:14 +0600

    Academic Semester Service -created

--- src/app/modules/academicSemester/academicSemester.validation.ts
+++ src/app/modules/academicSemester/academicSemester.validation.ts
@@ -7,7 +7,7 @@ const createAcademicSemesterValidationSchema = z.object({
     // password: z.string({ invalid_type_error: 'Password must be string' }).max(20, { message: 'Password can not be more than 20 characters' }).optional(),
     body: z.object({
         name: z.enum([...AcademicSemesterName] as [string, ...string[]]),
-        year: z.date(),
+        year: z.string(),
         code: z.enum([...AcademicSemesterCode] as [string, ...string[]]),
         startMonth: z.enum([...Months] as [string, ...string[]]),
         endMonth: z.enum([...Months] as [string, ...string[]]),
