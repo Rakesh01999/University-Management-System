4b7ef9375c37ebd6e4b33a4efaece96c4e014508
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-07 12:22:42 +0600

    Understanding Error Patterns in Zod and Mongoose

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,29 +1,55 @@
-import { Request, Response, NextFunction } from 'express';
-
-// const globalErrorHandler = (err: any, res: Response, req: Request, next: NextFunction) => {
-//     const statusCode = 500;
-//     const message = err.message || "Something went wrong !";
-
-//     return res.status(statusCode).json({
-//         success: false,
-//         message,
-//         error: err,
-//     })
-// };
+import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
 
 
-const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  const statusCode = err.statusCode || 500; // Use provided status code or default to 500
-  const message = err.message || "Something went wrong!";
 
-  res.status(statusCode).json({
+// const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+
+  // setting default values 
+  let statusCode = err.statusCode || 500; // Use provided status code or default to 500
+  let message = err.message || "Something went wrong!";
+
+  type TErrorSource = {
+    path: string | number;
+    message: string;
+  }[]
+
+  const errorSources = [{
+    path: '',
+    message: 'Something went wrong',
+  }];
+
+
+  if (err instanceof ZodError) {
+    statusCode = 400;
+    message = 'ami zod error';
+  }
+
+
+  // ultimate return 
+  return res.status(statusCode).json({
     success: false,
     message,
     // error: process.env.NODE_ENV === "development" ? err.stack : undefined, // Show stack in development mode
-    error:err,
+    errorSources,
+    error: err,
   });
 };
 
 
 
 export default globalErrorHandler;
+
+
+// pattern
+/*
+  success
+  message
+  errorSources: [
+    path: '',
+    message: ''  
+  ]
+
+  stack
+ */
