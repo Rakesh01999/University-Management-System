01a503f6dc64e707744ad845b61237e5e3f0c1bb
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-11 11:20:46 +0600

    semesterRegistration.interface  added

---
+++ src/app/modules/semesterRegistration/semesterRegistration.interface.ts
@@ -0,0 +1,14 @@
+
+
+import { Types } from 'mongoose';
+
+export type TSemesterRegistration = {
+  academicSemester: Types.ObjectId;
+  status: 'UPCOMING' | 'ONGOING' | 'ENDED';
+  startDate: Date;
+  endDate: Date;
+  minCredit: number;
+  maxCredit: number;
+};
+
+
