35190b46badb47cab3b51bbca75676c43ae42d4b
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-14 09:08:14 +0600

    user.model  modified

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -58,7 +58,7 @@ userSchema.post('save', function (doc, next) {
 
 
 userSchema.statics.isUserExistsByCustomId = async function (id: string) {
-    return await User.findOne({ id });
+    return await User.findOne({ id }).select('+password');
 };
 
 
