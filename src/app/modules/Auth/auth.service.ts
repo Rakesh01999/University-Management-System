793b8f4ce688f1d47736c12bbbb5d3538ce798ca
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-14 11:01:37 +0600

    auth.service, faculty.route -modified, cookie-parser installed

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -4,11 +4,11 @@
 import bcrypt from 'bcrypt';
 import httpStatus from 'http-status';
 import jwt, { JwtPayload } from 'jsonwebtoken';
-import config from '../../config';
 import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
 import { createToken } from './auth.utils';
 import AppError from '../../error/AppError';
+import config from '../../config';
 
 const loginUser = async (payload: TLoginUser) => {
   // checking if the user is exist
