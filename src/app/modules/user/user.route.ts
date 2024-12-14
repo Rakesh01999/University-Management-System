8f1dc9ede7331ddeadc3c2586e6f8311f2b7eceb
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-14 11:59:31 +0600

    bug fixed

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -11,7 +11,10 @@ import { USER_ROLE } from './user.constant';
 const router = express.Router();
 
 // router.post('/create-student', validateRequest(studentValidations.studentValidationSchema), UserControllers.createStudent);
+
+// router.post('/create-student', validateRequest(createStudentValidationSchema), UserControllers.createStudent);
 router.post('/create-student', auth(USER_ROLE.admin), validateRequest(createStudentValidationSchema), UserControllers.createStudent);
+
 router.post('/create-faculty', auth(USER_ROLE.admin), validateRequest(createFacultyValidationSchema), UserControllers.createFaculty);
 
 router.post('/create-admin',
